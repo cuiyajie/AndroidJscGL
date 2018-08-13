afd4aaeecd3e12c2405d5c706b549b41ca956056
Cuiyajie <cuiyajie_dlut@hotmail.com>
2018-08-14 01:35:30 +0800

    Init

---
+++ app/src/main/asssets/gl-sample.js
@@ -0,0 +1,3 @@
+gl.clearColor(255.0, 255.0, 0.0, 1.0)
+
+gl.clear(gl.COLOR_BUFFER_BIT)
---
+++ app/src/main/java/fex/momo/com/androidjscgl/MainActivity.java
@@ -0,0 +1,28 @@
+package fex.momo.com.androidjscgl;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("libjsc");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GLSurfaceView glSurfaceView = findViewById(R.id.glSurfaceView);
+    }
+
+    /**
+     * A native method that is implemented by the 'main' native library,
+     * which is packaged with this application.
+     */
+    public native void stringFromJNI();
+}
